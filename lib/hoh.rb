e527c8c7d87efd763b2eb8767ff2a04a65726726
David Holmes <phoenix1988@gmail.com>
2019-12-27 14:43:26 +0000

    Automatically backed up by Learn

--- lib/hoh.rb
+++ lib/hoh.rb
@@ -3,34 +3,41 @@
 # that HoH as the return value for the method "naming_system."
 #
 # The tests will guide your construction.
-#
-# PORTION_1 = {
-#   label: "Kingdom",
-#   sub_category: {
-#     label: "Phylum",
-#     sub_category: {
-#       label: "Class"
-#     }
-#   }
-# }
-# 
-# PORTION_2 = {
-#   label: "Order"
-# }
-# 
-# PORTION_3 = {
-#   label: "Family",
-#   sub_category: {
-#     label: "Genus",
-#   }
-# }
-# 
-# PORTION_4 = {
-#   label: "Species",
-#   sub_category: nil
-# }
+
+PORTION_1 = {
+  label: "Kingdom",
+  sub_category: {
+      label: "Phylum",
+      sub_category: {
+          label: "Class"
+          }
+      }
+  }
+ 
+PORTION_2 = {
+   label: "Order"
+ }
+ 
+ PORTION_3 = {
+   label: "Family",
+   sub_category: {
+     label: "Genus",
+   }
+ }
+ 
+ PORTION_4 = {
+   label: "Species",
+   sub_category: nil
+ }
 
 def naming_system
+  naming_system = {
+    PORTION_1,
+    PORTION_2,
+    PORTION_3,
+    PORTION_4
+  }
+  naming_system
   # Remember:
   #  Kingdom
   #  Phylum
